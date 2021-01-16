057e86859525a966ac6477a174406e5942a2ab54
Jessica Kerr <jessitron@gmail.com>
2021-01-16 22:22:39 +0000

    Close to a correct chart

--- main.ts
+++ main.ts
@@ -8,48 +8,6 @@ function logistic_map(r: R) {
 
 type IterateMap = (x: number) => number;
 
-const canvas = (document.getElementById("chart1") as HTMLCanvasElement).getContext("2d");
-if (!canvas) {
-  throw "poo";
-} else {
-  new Chart(canvas, {
-    type: 'bar',
-    data: {
-      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-      datasets: [{
-        label: '# of Votes',
-        data: [12, 19, 3, 5, 2, 3],
-        backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-          'rgba(255, 159, 64, 0.2)'
-        ],
-        borderColor: [
-          'rgba(255, 99, 132, 1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)'
-        ],
-        borderWidth: 1
-      }]
-    },
-    options: {
-      scales: {
-        yAxes: [{
-          ticks: {
-            beginAtZero: true
-          }
-        }]
-      }
-    }
-  });
-}
-
 function step(fn: IterateMap, x: number, n: Steps, subscript: Steps = 0): number {
   if (n <= 0) {
     return x;
@@ -60,4 +18,41 @@ function step(fn: IterateMap, x: number, n: Steps, subscript: Steps = 0): number
 
 console.log("Problem 1: " + step(logistic_map(2.5), 0.5, 3));
 console.log("Problem 2: " + step(logistic_map(2.6), 0.2, 10));
+
+
+// Homework 1.4
+
+const canvas = (document.getElementById("chart1") as HTMLCanvasElement).getContext("2d");
+if (!canvas) {
+  throw "poo";
+} else {
+  new Chart(canvas, {
+    type: 'scatter',
+    data: {
+      datasets: [{
+        label: 'r = 2, x0 = 0.2',
+        data: [{ x: 0, y: 0.2 }, { x: 1, y: 0.5 }],
+        backgroundColor: "red",
+        borderColor: "brown",
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        xAxes: [{
+          type: 'linear',
+          position: 'bottom',
+        }],
+        yAxes: [{
+          type: 'linear',
+          ticks: {
+            beginAtZero: true,
+            suggestedMin: 0,
+            suggestedMax: 1.0,
+          }
+        }]
+      }
+    }
+  });
+}
 
